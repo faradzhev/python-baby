e5f0476947a93dcaa67dbb64ac3cf697f7c152b4
Tymur Faradzhev <faradzhev@users.noreply.github.com>
2017-09-21 14:28:24 +0300

    lab2_2 shitty commit

---
+++ hello_2_2.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+name1, name2, name3 = input('Напишіть мені ваше прізвище, імя та по-батькові: ').split(sep=' ')
+print('Привіт, {} {} {}!'.format(name1, name2, name3)))
