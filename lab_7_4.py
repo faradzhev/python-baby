84ec9565dfc704df175caad56e1960f96cb53947
Tymur.org <me@tymur.org>
2017-10-05 14:42:30 +0300

    Lab7.5

--- lab_7_4.py
+++ lab_7_4.py
@@ -4,13 +4,11 @@
 def shchar(s) :
 	sNew = ''
 	alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a']
+	alphabet += [a.upper() for a in alphabet]	
 	for c in s :
 		for a in alphabet :
 			if c == a :
 				sNew += alphabet[alphabet.index(a)+1]
-				break
-			elif c == a.upper() :
-				sNew += alphabet[alphabet.index(a)+1].upper()
 				break
 		else:
 			sNew += c
