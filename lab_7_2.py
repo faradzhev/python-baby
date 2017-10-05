a74d07ff64a66bd0b98800fdd83efe862d5df375
Tymur.org <me@tymur.org>
2017-10-05 10:49:40 +0300

    Lab7.2 upd

--- lab_7_2.py
+++ lab_7_2.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+import re
 
 def ispalindr(s):
 	s = s.lower()
-	s = s.replace(" ", "")
+	s = re.sub('\W+', '', s)
+	print(s)
 	return True if s == s[::-1] else False
 
 s = input("Enter string: ")
