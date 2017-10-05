bf7dae3a58b4190a9673b3d0fac8d1e36d8564a3
Tymur.org <me@tymur.org>
2017-10-05 14:04:55 +0300

    Lab7.4

---
+++ lab_7_4.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def shchar(s) :
+	sNew = ''
+	alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a']
+	for c in s :
+		for a in alphabet :
+			if c == a :
+				sNew += alphabet[alphabet.index(a)+1]
+				break
+			elif c == a.upper() :
+				sNew += alphabet[alphabet.index(a)+1].upper()
+				break
+		else:
+			sNew += c
+	return sNew
+
+s = input("Enter string: ")
+print("Result: {}".format(shchar(s)))
