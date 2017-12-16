622c711dac7f5946f513071f0d24afb664ba4913
Tymur Faradzhev <me@tymur.org>
2017-12-16 20:57:36 +0200

    Lab7.7

--- lab_7_7.py
+++ lab_7_7.py
@@ -1,16 +1,27 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-def swap(s) :
-	sn = []
-	words = s.split(' ')
-	for w in words :
-		for i, c in enumerate(list(w)) :
-			if '_' in w and w[0] != '_' and w[-1] != '_' :
-				sn += '{}'.format(c.upper()) if c.isupper() else c
-			else:
-				sn += '_{}'.format(c.lower()) if c.isupper() else c
-			
-	return ''.join(sn)
+import re
 	
-print("Result: {}".format(swap(input("Enter string: "))))
+def toSnake(s) :
+	sr = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', s)
+	return re.sub('([a-z0-9])([A-Z])', r'\1_\2', sr).lower()
+	
+def toCamel(s) :
+	res = []
+	for word in s.split(' ') :
+		ws = word.split('_')
+		res.append( ws[0] + ''.join( i.title() for i in ws[1:] ) )
+	return ' '.join(res)
+
+def app():
+	s = input("Enter string: ")
+	method = input("Select method:\n1 - convert variables to camelCase\n2 - convert variables to snake_case\n")
+	if method == '1' :
+		res = toCamel(s)
+	elif method == '2' :
+		res = toSnake(s)
+	else :
+		res = "Unknown method, try again"
+	print(res)
+
+app()
