3de4c5f44981cca03ba45e73c4c4e919127bcf3f
Tymur Faradzhev <me@tymur.org>
2017-12-18 00:29:31 +0200

    Lab9.2

--- lab9_2.py
+++ lab9_2.py
@@ -49,6 +49,7 @@ hunwords = {
 }
 
 thowords = {
+	0: 'тисяч',
 	1: 'тисяча',
 	2: 'тисячі',
 	3: 'тисячі',
@@ -60,23 +61,67 @@ thowords = {
 	9: 'тисяч'
 }
 
+uahwords = {
+	0: 'гривень',
+	1: 'гривня',
+	2: 'гривні',
+	3: 'гривні',
+	4: 'гривні',
+	5: 'гривень',
+	6: 'гривень',
+	7: 'гривень',
+	8: 'гривень',
+	9: 'гривень'
+}
+
+coinwords = {
+	0: 'копійок',
+	1: 'копійка',
+	2: 'копійки',
+	3: 'копійки',
+	4: 'копійки',
+	5: 'копійок',
+	6: 'копійок',
+	7: 'копійок',
+	8: 'копійок',
+	9: 'копійок'
+}
+
 def num_to_words(num):
 	num = str(float(num)).split(".")
 	fiata = [int(n) for n in num[0]]
-	coins = ''.join(list(num[1])[:2])
-	kostyl = int(''.join(num[-2:]))
+	if len(num[1]) < 2: 
+		num[1] += '0'
+	coins = num[1][:2]
+	kostyl = int(num[0][-2:])
 	text = ''
-	for f in fiata[::-1] :
-		text = '{}'.format(onewords[kostyl]) if kostyl < 20 else '{} {}'.format(tenwords[fiata[-2]],onewords[fiata[-1]]) #+ text
-		#text = '{}'.format(onewords[f]) + text
-		
-		try:
-		except:
-			break
 	
-	return "{} {} {} копійок".format(text, "гривні" if not (kostyl < 20 and kostyl > 10) and fiata[-1] < 5 else "гривень", coins)
+	if len(fiata) > 1:
+		text = '{}'.format(onewords[kostyl]) if kostyl < 20 else '{} {}'.format(tenwords[fiata[-2]], onewords[fiata[-1]])
+	else:
+		text = '{}'.format(onewords[fiata[-1]])
+	if len(fiata) > 2:
+		text = '{}'.format(hunwords[fiata[-3]]) + ' ' + text
+	if len(fiata) > 3:
+		if len(fiata) > 4:
+			kostyl2 = int(''.join(list(map(str, fiata[-5:-3]))))
+			text = '{} {}'.format(onewords[kostyl2], thowords[9]) + ' ' + text if kostyl2 < 20 else '{} {} {}'.format(tenwords[fiata[-5]], onewords[fiata[-4]], thowords[fiata[-4]]) + ' ' + text
+		else:
+			text = '{} {}'.format(onewords[fiata[-4]], thowords[fiata[-4]]) + ' ' + text
+	if len(fiata) > 5:
+		text = '{}'.format(hunwords[fiata[-6]]) + ' ' + text
+	
+	return "{} {} {} {}".format(text, uahwords[fiata[-1]] if not (kostyl < 20 and kostyl > 10) else "гривень", coins, coinwords[int(coins[-1])])
 
 
 
 
-print(num_to_words("1234.56"))
+print(num_to_words("0.10"))
+print(num_to_words("13.19"))
+print(num_to_words("21.32"))
+print(num_to_words("365.54"))
+print(num_to_words("2467.75"))
+print(num_to_words("15648.16"))
+print(num_to_words("54183.87"))
+print(num_to_words("322234.56"))
+print(num_to_words("999999.99"))
